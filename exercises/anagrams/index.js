cd01c4d9fe528a081f2da18ad9a7ea25ab0e6341
Feng Hua <frankfenghua@gmail.com>
2020-06-09 08:45:59 -0400

    [exercises] anagrams cleanup

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -18,52 +18,46 @@ function anagrams(stringA, stringB) {
     let lena = a.length;
     let lenb = b.length;
 
-    let mapa = {};
-    let mapb = {};
+    let mapa = buildChapMap(a);
+    let mapb = buildChapMap(b);
     
-    a.split('').forEach(x => {
-        if(!mapa[x]){
-            mapa[x]=1;
-        }else{
-            mapa[x]++;
-        }
-    });
-
-    b.split('').forEach(x => {
-        if(!mapb[x]){
-            mapb[x]=1;
-        }else{
-            mapb[x]++;
-        }
-    });
-
     console.table(mapa); 
     console.table(mapb);
  
     if(lena>=lenb){
-        Object.keys(mapa).forEach(x=>{
-            let bkeys = Object.keys(mapb); 
-            let find = bkeys.filter(a=>a===x).length;
-            if(find>0){
-                result = result&& (mapa[x]===mapb[x]);
-            }else{
-                result = false;
-            }
-        })
+        result = compareChapMap(mapa,mapb);
     }else{
-        Object.keys(mapb).forEach(x=>{
-            let akeys = Object.keys(mapa); 
-            let find = akeys.filter(b=>b===x).length;
-            if(find>0){
-                result = result&& (mapa[x]===mapb[x]);
-            }else{
-                result = false;
-            }
-        })
- 
+        result = compareChapMap(mapb,mapa);
     } 
     console.warn('res',result);
     return result ;
+}
+
+function compareChapMap(mapa,mapb){
+    let result=true;
+    Object.keys(mapa).forEach(x=>{
+        let bkeys = Object.keys(mapb); 
+        let find = bkeys.filter(a=>a===x).length;
+        if(find>0){
+            result = result&& (mapa[x]===mapb[x]);
+        }else{
+            result = false;
+        }
+    })
+    return result;
+}
+
+function buildChapMap(str){
+    let mapChar={}
+    str.split('').forEach(x => {
+        if(!mapChar[x]){
+            mapChar[x]=1;
+        }else{
+            mapChar[x]++;
+        }
+    });
+    return mapChar
+
 }
 anagrams('rail safety', 'fairy talea')
 module.exports = anagrams;
