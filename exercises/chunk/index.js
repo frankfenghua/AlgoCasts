c0b70375a7f4c111d500a9db87e27015ff3807c2
Feng Hua <frankfenghua@gmail.com>
2020-06-09 03:52:25 -0400

    [exercises] chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let tmp = []; 
+    let result = []; 
+    let a = [...array];  
+    let len = a.length;  
+    let count = Math.floor(len/size);
+    if(len % size !== 0 ){
+        count++;
+    }
+    console.warn('count',count);
+    console.warn('array apread',...array);
 
+    for( let i = 0 ; i< len; i+=size){ 
+        console.warn('i',i);
+        tmp = a.splice(0,size);
+        result.push(tmp);    
+    }  
+    for (let i = 0 ; i< result.length; i++)  {
+        console.warn(`result[${i}]=${result[i]}`);
+    }  
+    return result;    
+}
+function chunkThem(array, size) {
+    const chunked = [];
+    let index = 0;
+  
+    while (index < array.length) {
+      chunked.push(array.slice(index, index + size));
+      index += size;
+    }
+  
+    return chunked;
+  }
+chunk([1,2,3,4],2)
 module.exports = chunk;
