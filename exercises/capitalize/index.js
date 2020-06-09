d1d3cc2747f510a4d858a87c1b774b75eb3c2d9a
Feng Hua <frankfenghua@gmail.com>
2020-06-09 09:29:06 -0400

    [exercises] capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let arr = str.split(' ');
+    console.warn('0 - str',str);
+    console.warn('1 - arr',arr);
 
+    for( let i = 0 ; i< arr.length; i++){
+        let chars = arr[i].split('');
+        let firstChar = chars[0].toUpperCase();
+        chars[0] = firstChar;
+        console.warn('2 - chars',chars);
+        arr[i] = chars.join('');
+        
+        console.warn('3 - arr',arr[i]);
+
+    } 
+    console.warn('4 - arr',arr);     
+    
+    let result = arr.join(' ');
+    console.warn('5 - res',result);
+    return result;
+
+}
+capitalize('look, it is working!');
+console.warn('end',);
 module.exports = capitalize;
